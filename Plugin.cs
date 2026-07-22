7dbf191d54d598f4f08e8ac3c5f256cd49dd8272
AceDev <masonbrackin23@gmail.com>
2026-07-21 21:55:26 -0400

    Add constants for plugin configuration

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace AceCastingMod;
+
+public static class Constants
+{
+    public const string PluginGuid = "ace.CastingMod.plugin.AceCastingMod";
+    public const string PluginName = "AceCastingMod";
+    public const string PluginVersion = "1.0.5";
+
+    public static readonly string MenuSubtitle = "AceCastingMod\n" +
+                                                 "Made By Ace With love ❤️\n";
+
+    public const string PluginDescription = "ACM best Casting Mod ever";
+}
